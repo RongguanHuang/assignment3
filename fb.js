8f0edf74a378a0e5b63e043a6bc218ac7d52a143
RongguanHuang <rhuang1@mail.sfsu.edu>
2016-04-26 02:15:42 +0000

    assignment 3 completed

---
+++ fb.js
@@ -0,0 +1,6 @@
+// facebook app settings - fb.js
+module.exports = {
+  'appID' : '1756735051211430',
+  'appSecret' : '0fb7405d08fc870e64962c46e3d8f7a5',
+  'callbackUrl' : 'https://assignment-no1-rongguanhuang.c9users.io/login/facebook/callback'
+}
---
+++ linkd.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'appID' : '75b14tvj5qess7',
+  'appSecret' : 'jo1M4yZ5a8pCo7Ra',
+  'callbackUrl' : 'https://assignment-no1-rongguanhuang.c9users.io/login/linkedin/callback'
+}
